0cdc1cfc4a77769bd79103e6cc96e14760f3b01a
Caylipp <philipphoang3005@gmail.com>
2024-01-23 23:00:28 -0600

    -created tap button -created optional toast

--- app/src/main/java/com/hoang/msu/counter/MainActivity.kt
+++ app/src/main/java/com/hoang/msu/counter/MainActivity.kt
@@ -2,10 +2,27 @@ package com.hoang.msu.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tapButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        tapButton = findViewById(R.id.tap_button)
+
+        tapButton.setOnClickListener{view : View ->
+            Toast.makeText(
+                this,
+                R.string.tap_button,
+                Toast.LENGTH_SHORT)
+                .show()
+        }
     }
+
+
 }
