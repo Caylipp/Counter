5fb9402893a606a24c388a248c853e96560b471b
Caylipp <philipphoang3005@gmail.com>
2024-01-24 15:12:09 -0600

    -refinement

--- app/src/main/java/com/hoang/msu/counter/MainActivity.kt
+++ app/src/main/java/com/hoang/msu/counter/MainActivity.kt
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 
 
 class Counter{
 
-    private var count : Int = 0;
+    private var count : Int = 0
     fun addCount(){
         count++
     }
@@ -32,13 +31,6 @@ class MainActivity : AppCompatActivity() {
         tapButton.setOnClickListener{
             count.addCount()
             display.text = count.getCount().toString()
-            /*
-            Toast.makeText(
-                this,
-                R.string.tap_button,
-                Toast.LENGTH_SHORT
-            ).show()
-            */
         }
     }
 
