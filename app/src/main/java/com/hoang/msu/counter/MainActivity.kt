b0f56cea6d5f6bcee1ff73ec1583a913abd484a2
Caylipp <philipphoang3005@gmail.com>
2024-01-24 14:35:29 -0600

    -implement logic for counter -created counter class

--- app/src/main/java/com/hoang/msu/counter/MainActivity.kt
+++ app/src/main/java/com/hoang/msu/counter/MainActivity.kt
@@ -4,25 +4,44 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
+
+class Counter{
+
+    private var count : Int = 0;
+    fun addCount(){
+        count++
+    }
+    fun getCount() : Int {
+        return count
+    }
+}
 class MainActivity : AppCompatActivity() {
     private lateinit var tapButton : Button
+    private lateinit var display : TextView
+    private var count : Counter = Counter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
         tapButton = findViewById(R.id.tap_button)
+        display = findViewById(R.id.textView)
 
         tapButton.setOnClickListener{view : View ->
+            count.addCount()
+            display.text = count.getCount().toString()
             Toast.makeText(
                 this,
                 R.string.tap_button,
                 Toast.LENGTH_SHORT)
+
                 .show()
         }
     }
+
 
 
 }
